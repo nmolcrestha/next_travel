d7a07eb58bfa06fff52f46538d7df81d988a55e8
Anmol Shrestha <nmol.crestha@gmail.com>
2023-10-16 07:16:16 +0545

    Components init

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section className='max-container padding-container flx flex-col gap-20 py-10 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500'>
+      <div className='hero-map'>
+
+      </div>
+    </section>
+  )
+}
