c168081ca7927bf39fa2feb1f85bc115e95880bf
Anmol Shrestha <nmol.crestha@gmail.com>
2023-10-19 20:14:27 +0545

    features component fix

--- components/Features.tsx
+++ components/Features.tsx
@@ -6,7 +6,7 @@ export const Features = () => {
   return (
     <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
       <div className="max-container padding-container relative w-full flex justify-end">
-        <div>
+        <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
             alt="phone"
@@ -15,7 +15,7 @@ export const Features = () => {
             className="feature-phone"
           />
         </div>
-        <div className="z-20 w-full flex-col lg:w-[60%]">
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
           <div className="relative">
             <Image
               src="/camp.svg"
@@ -55,11 +55,11 @@ const FeatureItem = ({ title, icon, variant, description }: FeatureItem) => {
     <li className="flex w-full flex-1 flex-col items-start">
       <div className="rounded-full p-4 lg:p-7 bg-green-50">
         <Image src={icon} alt="map" width={28} height={28} />
+      </div>
         <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
         <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
           {description}
         </p>
-      </div>
     </li>
   );
 };
